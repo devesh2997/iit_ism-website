1ac0d4955ed613039a313ab666dafa76f5c29b68
vipinsoni38@gmail.com <vipinsoni38@gmail.com>
2018-05-17 13:37:04 +0530

    notice navigation

---
+++ js/homepage.js
@@ -0,0 +1,19 @@
+function navigation_notices(no) {
+	var name;
+	switch(no){
+		case 0:name="Short courses"; break;
+		case 1:name="Exam/PhD"; break;
+		case 2:name="Notices"; break;
+		case 3:name="Seminar"; break;
+		case 4:name="Other info"; break;
+		default : name="Notices";
+
+	}
+	document.getElementById("notices").innerHTML ="<h3>"+ name+"</h3>";
+	document.getElementById("notices-inner-stuff").innerHTML ="<h3> "+ name+"</h3>";
+	
+	
+}
+
+
+
