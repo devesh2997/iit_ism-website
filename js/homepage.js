80cf15f775ec35c8d4bf25431976affac5c6dc2e
vipinsoni38@gmail.com <vipinsoni38@gmail.com>
2018-05-31 23:44:33 +0530

    home page improved

--- js/homepage.js
+++ js/homepage.js
@@ -1,3 +1,25 @@
+// Google Expando Method
+// =====================================================
+
+function googleExpandoToggle() {
+  $(this).toggleClass('active');
+  $(this).next().toggleClass('active');
+  $expando_card = $('.google-expando__card');
+
+  // ARIA
+  $expando_card.attr('aria-hidden') === 'true' ? $expando_card.attr('aria-hidden', 'false') : $expando_card.attr('aria-hidden', 'true');
+}
+
+
+// Google Expando Event
+// =====================================================
+
+$('.google-expando__icon').on('click', function() {
+  googleExpandoToggle.call(this);
+});
+
+
+
 function navigation_notices(no) {
 	var name;
 	switch(no){
